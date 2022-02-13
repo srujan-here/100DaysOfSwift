4d7bdb18eb839b72b591bf52788ef666654c4c56
Putta Srujan <93040995+srujan-here@users.noreply.github.com>
2022-02-13 19:48:05 +0530

    Constants,variables,string interpolations

---
+++ Day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Cocoa
+
+var a="srujan"
+var t=1_00
+
+var str="""
+hai u
+how are u
+and how do u do
+"""
+
+var t1="""
+hai u\
+how are u\
+and how do u do\
+
+"""
+
+var s=1.3
+var tm=true
+
+
+var score=85
+var myscore="my score id \(score)"
+var result="my result is \(myscore)"
+
+
+//constants
+
+let dc=20
+
+var sales: Double = 100_000
+var siri: Int = 2000
+var mg:String="sksk"
